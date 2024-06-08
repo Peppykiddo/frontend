404a92ee9a39d696d618ca60f36d9c8c0bbd216a
Peppykiddo <tejaswinikalyani6@gmail.com>
2024-06-08 20:50:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React,{useState} from "react"; 
+
+import { Route,Routes,Link } from "react-router-dom"; 
+
+import Navbar from "./Navbar";
+
+import { Register } from './Register';
+import './Registration.css';
+import './App.css';
+import { Login } from './Login';
+import UpdatematerialReceipt from './Updatematerialreceipts';
+import Updateothertable from './Updateothertable';
+import Instructions from './Instructions';
+import Formtable from './Formtable';
+
+import FormDataResult from "./FormDataResult"; 
+import EMD from "./EMD";
+function App() {
+const [currentForm,setCurrentForm]= useState('login'); 
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+   }
+  return (
+
+    <><div>
+      <Routes>
+        <Route path="/updatematerial" element={<UpdatematerialReceipt />} />
+        <Route path="/othertables" element={<Updateothertable />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/Formtable" element={<Formtable />} />
+        <Route path="/EMD" element={<EMD />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="*" Component={<h1>404 Not Found</h1>}  />
+
+      </Routes>
+
+    </div>
+    
+    {/* <div>
+        {currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Formtable onFormSwitch={toggleForm} />}
+
+
+
+      </div> */}
+      
+      </>
+      
+
+
+  );
+}
+
+export default App;
