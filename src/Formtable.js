3c01fbac59cb21038a6928f196d76403d2d7ac0b
Peppykiddo <tejaswinikalyani6@gmail.com>
2024-06-10 17:17:42 +0530

    change endpoint

--- src/Formtable.js
+++ src/Formtable.js
@@ -36,7 +36,7 @@ const Formtable = () => {
         fromDate: new Date(formData.fromDate).toISOString(),
         toDate: new Date(formData.toDate).toISOString()
       };
-      const response = await axios.post('http://52.201.254.65:3000/user/postData', formattedData); // Corrected API endpoint URL
+      const response = await axios.post('http://184.73.62.4:3000/user/postData', formattedData); // Corrected API endpoint URL
       console.log('Response:', response.data);
       setSubmittedData(formattedData);
       setShowResult(true);
--- src/Login.js
+++ src/Login.js
@@ -17,7 +17,7 @@ export const Login = (props) => {
         setError(''); // Clear previous errors
 
         try {
-            const response = await axios.post('http://52.201.254.65:3000/login/api/login', {
+            const response = await axios.post('http://184.73.62.4:3000/login/api/login', {
                 email,
                 password: pass
             });
