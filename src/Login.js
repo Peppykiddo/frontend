ee448a5f1276dc4b192d51f0db0c563c1bab5e2c
Peppykiddo <tejaswinikalyani6@gmail.com>
2024-06-25 15:14:38 +0530

    deploy changes

--- src/Login.js
+++ src/Login.js
@@ -17,7 +17,7 @@ export const Login = (props) => {
         setError(''); // Clear previous errors
 
         try {
-            const response = await axios.post('http://54.196.55.108:3000/login/api/login', {
+            const response = await axios.post('http://54.87.232.77:3000/login/api/login', {
                 email,
                 password: pass
             });
